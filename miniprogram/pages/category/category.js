cdc10d00971b2a78f327c6dcddc2506ccc5bdadf
A24691 <luoweijia@autel.com>
2025-05-12 19:50:19 +0800

    分类界面逻辑提交

--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -16,12 +16,28 @@ Page({
 
   // 获取商品分类的数据
   async getCategoryData() {
-    // const res = await reqCategoryData()
-    // 此处获取分类列表，以及每个分类下的商品，使用goodlist包裹
-    if (res.code === 200) {
-      this.setData({
-        categoryList: res.data
-      })
+    try {
+      // 调用 listHomePage 云函数来获取所有数据
+      const res = await wx.cloud.callFunction({
+        name: 'getCategoryGoodData' // 替换为你的云函数名称
+      });
+
+      // 检查云函数返回的结果
+      if (res.result && res.result.success) {
+        this.setData({
+          categoryList: res.result.data
+        })
+      } else {
+        wx.showToast({
+          title: '获取分类数据失败',
+        });
+      }
+    } catch (error) {
+      // 捕获并处理调用云函数时的错误
+      wx.showToast({
+        title: '调用云函数获取分类数据失败',
+      });
+      console.error('Error calling listHomePage:', error);
     }
   },
 
